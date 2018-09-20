ec56101f7e7ed48c4f9369d56b970d9f9c0f7db8
Flavie Laurens <flavilo@hotmail.fr>
2018-09-20 14:11:55 +0200

    enhancement (introduction): display introduction content with sidebar

--- src/app/introduction/introduction.component.ts
+++ src/app/introduction/introduction.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiServiceResult, ConvertJSONLD, ReadResourcesSequence, SearchService } from '@knora/core';
+import {
+    ApiServiceResult,
+    ConvertJSONLD,
+    KnoraConstants,
+    OntologyInformation,
+    ReadResource,
+    ReadResourcesSequence,
+    SearchService,
+    ResourceService,
+    OntologyCacheService,
+    ApiServiceError,
+    IncomingService,
+    StillImageRepresentation
+} from '@knora/core';
 import { BeolService } from '../services/beol.service';
 import { ActivatedRoute, Params } from '@angular/router';
 
@@ -14,25 +27,40 @@ let jsonld = require('jsonld');
 export class IntroductionComponent implements OnInit {
 
     id: string;
+    iri: string;
     project: string;
+    isbn: string;
+    resource: ReadResource;
+    ontologyInfo: OntologyInformation;
+
+    KnoraConstants = KnoraConstants;
 
     constructor(private _route: ActivatedRoute,
         private _searchService: SearchService,
-        private _beol: BeolService) {
+        private _beol: BeolService,
+        private _resourceService: ResourceService,
+        private _cacheService: OntologyCacheService,
+        private _incomingService: IncomingService) {
     }
 
     ngOnInit() {
+        this.isbn = '978-3-0348-0880-4';
+        /* this.iri = 'http://rdfh.ch/0801/9rPI9QnWSsGsDeGa_5v5Iw'; */
         this._route.params.subscribe((params: Params) => {
             this.project = params['project'];
             // console.log('project', this.project);
             this.id = params['id'];
             // console.log('id ', this.id);
         });
+
+        this.searchForBook(this.isbn, this.id);
+
+        /* this.requestResource(this.iri); */
     }
 
-    searchForBook(isbn: string, id: string) {
+    searchForBook(isbn: string, id: string): void {
 
-        const gravsearch: string = this._beol.searchForBook(isbn, id);
+        const gravsearch: string = this._beol.searchForBookById(isbn, id);
 
         this._searchService.doExtendedSearch(gravsearch).subscribe(
             (result: ApiServiceResult) => {
@@ -46,13 +74,8 @@ export class IntroductionComponent implements OnInit {
                     const resourceSeq: ReadResourcesSequence = ConvertJSONLD.createReadResourcesSequenceFromJsonLD(compacted);
 
                     if (resourceSeq.resources.length === 1) {
-
-                        console.log('we got a resource');
-
-                        // const config: MatDialogConfig = ObjectDialogComponent.createConfiguration(resourceSeq.resources[0].id);
-
-                        // this.dialog.open(ObjectDialogComponent, config);
-
+                        console.log('we got a resource sequence ', resourceSeq.resources);
+                        this.requestResource(resourceSeq.resources[0].id);
                     }
 
                 }, function (err) {
@@ -63,6 +86,72 @@ export class IntroductionComponent implements OnInit {
             }
         );
 
+    }
+
+    /**
+     * Requests a resource from Knora.
+     *
+     * @param iri the Iri of the resource to be requested.
+     */
+    private requestResource(iri: string): void {
+        this._resourceService.getResource(iri)
+            .subscribe(
+                (result: ApiServiceResult) => {
+                    const promises = jsonld.promises;
+                    // compact JSON-LD using an empty context: expands all Iris
+                    const promise = promises.compact(result.body, {});
+
+                    promise.then((compacted) => {
+
+                        const resourceSeq: ReadResourcesSequence = ConvertJSONLD.createReadResourcesSequenceFromJsonLD(compacted);
+
+                        // make sure that exactly one resource is returned
+                        if (resourceSeq.resources.length === 1) {
+
+                            // get resource class Iris from response
+                            const resourceClassIris: string[] = ConvertJSONLD.getResourceClassesFromJsonLD(compacted);
+
+                            // console.log(resourceClassIris)
+
+                            // request ontology information about resource class Iris (properties are implied)
+                            this._cacheService.getResourceClassDefinitions(resourceClassIris).subscribe(
+                                (resourceClassInfos: OntologyInformation) => {
+
+                                    // initialize ontology information
+                                    this.ontologyInfo = resourceClassInfos;
+
+                                    // prepare a possibly attached image file to be displayed
+                                    // ResourceObjectComponent.collectImagesAndRegionsForResource(resourceSeq.resources[0]);
+
+                                    this.resource = resourceSeq.resources[0];
+                                    console.log('resource properties: ', this.resource.properties);
+
+                                    // this.requestIncomingResources();
+                                },
+                                (err) => {
+
+                                    console.log('cache request failed: ' + err);
+                                });
+                        } else {
+                            // exactly one resource was expected, but resourceSeq.resources.length != 1
+                            // this.errorMessage =
+                            // `Exactly one resource was expected, but ${resourceSeq.resources.length} resource(s) given.`
+
+                        }
+
+                    }, function (err) {
+
+                        console.log('JSONLD of full resource request could not be expanded:' + err);
+                    });
+
+                    // this.isLoading = false;
+                },
+                (error: ApiServiceError) => {
+                    console.error(error);
+                    /* this.errorMessage = <any>error;
+                    this.isLoading = false; */
+                }
+            );
     }
 
 }
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiServiceResult, ConvertJSONLD, ReadResourcesSequence, SearchService } from '@knora/core';
+import { BeolService } from '../services/beol.service';
+import { ActivatedRoute, Params } from '@angular/router';
+
+declare let require: any; // http://stackoverflow.com/questions/34730010/angular2-5-minute-install-bug-require-is-not-defined
+let jsonld = require('jsonld');
 
 @Component({
   selector: 'app-landing-page',
@@ -7,7 +13,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
