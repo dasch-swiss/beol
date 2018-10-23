f2a8ae5f4c795357c31cef37e7b1c0175a839cba
Flavie Laurens <flavilo@hotmail.fr>
2018-10-23 16:29:37 +0200

    test (biblio): fix basic tests

--- src/app/resource/biblio-items/biblio-items.component.spec.ts
+++ src/app/resource/biblio-items/biblio-items.component.spec.ts
@@ -1,16 +1,48 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MaterialModule } from '../../material-module';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
+import { KuiActionModule } from '@knora/action';
+import { KuiCoreConfig } from '@knora/core';
 import { BiblioItemsComponent } from './biblio-items.component';
+import { ReadTextValueAsHtmlComponent } from '../../properties/read-text-value-as-html/read-text-value-as-html.component';
+import { ReadLinkValueComponent } from '../../properties/read-link-value/read-link-value.component';
+import { MathJaxDirective } from '../../directives/mathjax.directive';
 
 describe('BiblioItemsComponent', () => {
   let component: BiblioItemsComponent;
   let fixture: ComponentFixture<BiblioItemsComponent>;
 
+  const id = 'http://rdfh.ch/0802/eJ-JOOfoS4yvACkcTgZDfw';
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BiblioItemsComponent ]
+      imports: [
+        KuiActionModule,
+        MaterialModule,
+        RouterTestingModule,
+        HttpClientModule,
+        HttpClientTestingModule
+      ],
+      declarations: [
+        BiblioItemsComponent,
+        ReadTextValueAsHtmlComponent,
+        ReadLinkValueComponent,
+        MathJaxDirective
+      ],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: { params: of({ id }) }
+        },
+        { provide: 'config', useValue: KuiCoreConfig }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
