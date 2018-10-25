af89dbfd9ae3a71a4d69f8c3b3a6343c8c7a375e
Flavie Laurens <flavilo@hotmail.fr>
2018-10-25 09:31:01 +0200

    enhancement (resource): add a type to navigationSubscription

--- src/app/resource/resource.component.ts
+++ src/app/resource/resource.component.ts
@@ -22,16 +22,14 @@ import {
 } from '@knora/core';
 import { RequestStillImageRepresentations } from '@knora/viewer';
 import { BeolResource } from './beol-resource';
-
-declare let require: any; // http://stackoverflow.com/questions/34730010/angular2-5-minute-install-bug-require-is-not-defined
-let jsonld = require('jsonld');
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-resource',
     templateUrl: './resource.component.html',
     styleUrls: ['./resource.component.scss']
 })
-export class ResourceComponent extends BeolResource implements OnDestroy  {
+export class ResourceComponent extends BeolResource implements OnDestroy {
 
     iri: string;
     resource: ReadResource;
@@ -40,14 +38,14 @@ export class ResourceComponent extends BeolResource implements OnDestroy  {
     isLoading = true;
     errorMessage: any;
     KnoraConstants = KnoraConstants;
-    navigationSubscription;
+    navigationSubscription: Subscription;
 
     constructor(private _route: ActivatedRoute,
-                private _router: Router,
-                protected _resourceService: ResourceService,
-                protected _cacheService: OntologyCacheService,
-                protected _incomingService: IncomingService,
-                public location: Location) {
+        private _router: Router,
+        protected _resourceService: ResourceService,
+        protected _cacheService: OntologyCacheService,
+        protected _incomingService: IncomingService,
+        public location: Location) {
 
         super(_resourceService, _cacheService, _incomingService);
 
