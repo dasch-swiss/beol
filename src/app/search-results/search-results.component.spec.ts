0fe2ffbbb5a03f4258e15baabdb4c4855cc029ac
Tobias Schweizer <t.schweizer@unibas.ch>
2018-12-05 09:34:33 +0100

    test (search-results): mock services

--- src/app/search-results/search-results.component.spec.ts
+++ src/app/search-results/search-results.component.spec.ts
@@ -8,24 +8,35 @@ import { BehaviorSubject, Observable, of } from 'rxjs';
 import { SearchResultsComponent } from './search-results.component';
 import { KuiActionModule } from '@knora/action';
 
-import { KuiCoreConfig, SearchParamsService } from '@knora/core';
+import {
+    ApiServiceResult,
+    ExtendedSearchParams,
+    KuiCoreConfig,
+    OntologyCacheService, OntologyInformation, Properties, ResourceClasses,
+    SearchParamsService,
+    SearchService
+} from '@knora/core';
 import { KuiViewerModule } from '@knora/viewer';
 
 import { MathJaxDirective } from '../directives/mathjax.directive';
 import { MatExpansionModule, MatIconModule } from '@angular/material';
 
 
-describe('SearchResultsComponent', () => {
+fdescribe('SearchResultsComponent', () => {
     let component: SearchResultsComponent;
     let fixture: ComponentFixture<SearchResultsComponent>;
     const mode = 'extended';
     const q = 'test';
 
     let mockSearchParamService;
+    let searchServiceSpy: jasmine.SpyObj<SearchService>; // see https://angular.io/guide/testing#angular-testbed
+    let ontoCacheSpy: jasmine.SpyObj<OntologyCacheService>;
 
     beforeEach(async(() => {
 
         mockSearchParamService = new MockSearchParamsService();
+        const spySearchService = jasmine.createSpyObj('SearchService', ['doExtendedSearchCountQuery', 'doExtendedSearch']);
+        const spyOntoCache = jasmine.createSpyObj('OntologyCacheService', ['getResourceClassDefinitions']);
 
         TestBed.configureTestingModule({
             imports: [
@@ -47,10 +58,54 @@ describe('SearchResultsComponent', () => {
                     useValue: { params: of({ mode, q }) }
                 },
                 { provide: 'config', useValue: KuiCoreConfig },
-                { provide: SearchParamsService, useValue: mockSearchParamService}
+                { provide: SearchParamsService, useValue: mockSearchParamService},
+                { provide: SearchService, useValue: spySearchService },
+                { provide: OntologyCacheService, useValue: spyOntoCache }
             ]
         })
             .compileComponents();
+
+        searchServiceSpy = TestBed.get(SearchService);
+
+        searchServiceSpy.doExtendedSearchCountQuery.and.callFake((gravsearch: string) => {
+
+                const result = new ApiServiceResult();
+                result.status = 200;
+                result.statusText = '';
+                result.url = '';
+                result.body = require('../test-data/search-results/search-response-letters-count.json') // mock response
+
+                return of(
+                    result
+                );
+            }
+        );
+
+        searchServiceSpy.doExtendedSearch.and.callFake((gravsearch: string) => {
+
+            const result = new ApiServiceResult();
+            result.status = 200;
+            result.statusText = '';
+            result.url = '';
+            result.body = require('../test-data/search-results/search-response-letters.json'); // mock response
+
+            return of(
+                result
+            );
+        });
+
+        ontoCacheSpy = TestBed.get(OntologyCacheService);
+
+        ontoCacheSpy.getResourceClassDefinitions.and.callFake(() => {
+            const resClasses: ResourceClasses = require('../test-data/search-results/ontology-info-resource-classes.json');
+            const properties: Properties = require('../test-data/search-results/ontology-info-properties.json');
+
+            const ontoInfo = new OntologyInformation({}, resClasses, properties);
+
+            return of(
+                ontoInfo
+            );
+        });
     }));
 
     beforeEach(() => {
@@ -63,15 +118,31 @@ describe('SearchResultsComponent', () => {
         expect(component).toBeTruthy();
     });
 
+    it('should perform a count query', () => {
+        expect(searchServiceSpy.doExtendedSearchCountQuery).toHaveBeenCalledTimes(1);
+
+        expect(searchServiceSpy.doExtendedSearchCountQuery).toHaveBeenCalledWith('testquery0');
+    });
+
+
 });
 
 class MockSearchParamsService {
 
-    public currentSearchParams: Observable<any>;
+    private _currentSearchParams: BehaviorSubject<any>;
 
     constructor() {
-        this.currentSearchParams = new BehaviorSubject<any>(1);
+        this._currentSearchParams = new BehaviorSubject<ExtendedSearchParams>(new ExtendedSearchParams((offset: number) => 'testquery' + offset));
     }
+
+    changeSearchParamsMsg(searchParams: ExtendedSearchParams): void {
+        this._currentSearchParams.next(searchParams);
+    }
+
+    getSearchParams(): ExtendedSearchParams {
+        return this._currentSearchParams.getValue();
+    }
+
 
 
 }
