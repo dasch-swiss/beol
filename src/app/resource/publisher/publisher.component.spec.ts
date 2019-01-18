f08ae543537c35d1bbc176f0d5e2aea2c8ec64a7
Sepideh Alassi <sepideh.alassi@unibas.ch>
2019-01-18 15:58:53 +0000

    Publisher component & Publication ExternalLink (#97)
    * fix (fixLetterTemplate) properties isReplyTo and hasFigure should be treated as lists
    
    * fix : seperate Leonhardi Euleri Commercium Epistolicum from LEOO
    
    * fix (bibliographieTemplates) separate publisher component from person
    
    * feature (fixBibliographieTemplate) add property ExternalLink to bibliography items
    
    * fix (fixBibliographieTemplate) fix the test for publisher component
    
    * fix (fixBibliographieTemplate) show publishingPerson property

---
+++ src/app/resource/publisher/publisher.component.spec.ts
@@ -0,0 +1,64 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Location } from '@angular/common';
+import { MaterialModule } from '../../material-module';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { PublisherComponent } from './publisher.component';
+import { KuiActionModule } from '@knora/action';
+import { of } from 'rxjs';
+import { KuiCoreConfig } from '@knora/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ReadTextValueAsHtmlComponent } from '../../properties/read-text-value-as-html/read-text-value-as-html.component';
+import { MathJaxDirective } from '../../directives/mathjax.directive';
+import { KuiViewerModule } from '@knora/viewer';
+import { ReadTextValueComponent } from '../../properties/read-text-value/read-text-value.component';
+
+describe('PublisherComponent', () => {
+    let component: PublisherComponent;
+    let fixture: ComponentFixture<PublisherComponent>;
+    const id = 'http://rdfh.ch/0802/o2zZYlxUTnqPSx3pW0h7nQ'; // Olms Hildesheim id
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                KuiActionModule,
+                KuiViewerModule,
+                MaterialModule,
+                HttpClientModule,
+                HttpClientTestingModule,
+                RouterTestingModule
+            ],
+            declarations: [
+                PublisherComponent,
+                ReadTextValueAsHtmlComponent,
+                ReadTextValueComponent,
+                MathJaxDirective
+            ],
+            providers: [
+                {provide: Location},
+                {
+                    provide: ActivatedRoute,
+                    useValue: { paramMap: of({
+                            get: () => {
+                                return id;
+                            }
+                        })}
+                },
+                {provide: 'config', useValue: KuiCoreConfig}
+            ]
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(PublisherComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
