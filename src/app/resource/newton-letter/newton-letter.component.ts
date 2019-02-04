cd58fbff20efa7e77e71ca46df03621f9c53a830
Sepideh Alassi <sepideh.alassi@gmail.com>
2019-02-04 11:38:50 +0000

    fix (newton) semicolon at the end of import

--- src/app/resource/newton-letter/newton-letter.component.ts
+++ src/app/resource/newton-letter/newton-letter.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ActivatedRoute, ParamMap} from '@angular/router';
 import {Location} from '@angular/common';
-import {environment} from '../../../environments/environment',
+import {environment} from '../../../environments/environment';
 import {
     IncomingService,
     KnoraConstants,
