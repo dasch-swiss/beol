ca8bc26bc94876466d48d1138ffc540a5c8c26b1
Tobias Schweizer <t.schweizer@unibas.ch>
2019-02-06 15:53:20 +0100

    test (bebb route): mock dependencies

--- src/app/bebb-route/bebb-route.component.spec.ts
+++ src/app/bebb-route/bebb-route.component.spec.ts
@@ -4,16 +4,24 @@ import { BebbRouteComponent } from './bebb-route.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { KuiCoreConfig } from '@knora/core';
+import { KuiCoreConfig, ReadResource, ReadResourcesSequence, SearchService } from '@knora/core';
 import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
+import { BeolService } from '../services/beol.service';
 
 describe('BebbRouteComponent', () => {
     let component: BebbRouteComponent;
     let fixture: ComponentFixture<BebbRouteComponent>;
     const lt = '1706-03-17_Hermann_Jacob-Scheuchzer_Johannes';
 
+    let beolService: BeolService;
+    let searchService: SearchService;
+
     beforeEach(async(() => {
+
+        const beolServiceSpy = jasmine.createSpyObj('BeolService', ['searchForLetterFromBEBB', 'routeByResourceType']); // see https://angular.io/guide/testing#angular-testbed
+        const searchServiceSpy = jasmine.createSpyObj('SearchService', ['doExtendedSearchReadResourceSequence']);
+
         TestBed.configureTestingModule({
             imports: [
                 HttpClientModule,
@@ -32,10 +40,28 @@ describe('BebbRouteComponent', () => {
                         })
                     }
                 },
-                {provide: 'config', useValue: KuiCoreConfig}
+                {provide: 'config', useValue: KuiCoreConfig},
+                {provide: BeolService, useValue: beolServiceSpy},
+                {provide: SearchService, useValue: searchServiceSpy}
             ]
         })
             .compileComponents();
+
+        beolServiceSpy.searchForLetterFromBEBB.and.returnValue('gravsearchQuery');
+
+        beolService = TestBed.get(BeolService);
+
+        const mockRes = of(
+            new ReadResourcesSequence(
+                [new ReadResource('letterIri', 'http://0.0.0.0:3333/ontology/0801/beol/v2#letter', 'label', [], [], [], [], {})],
+                1
+            )
+        );
+
+        searchServiceSpy.doExtendedSearchReadResourceSequence.and.returnValue(mockRes);
+
+        searchService = TestBed.get(SearchService);
+
     }));
 
     beforeEach(() => {
@@ -46,5 +72,14 @@ describe('BebbRouteComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should perform a query to get the letter\'s actual Iri', () => {
+
+        expect(beolService.searchForLetterFromBEBB).toHaveBeenCalledWith(lt);
+
+        expect(searchService.doExtendedSearchReadResourceSequence).toHaveBeenCalledWith('gravsearchQuery');
+
+        expect(beolService.routeByResourceType).toHaveBeenCalledWith('http://0.0.0.0:3333/ontology/0801/beol/v2#letter', 'letterIri');
     });
 });
