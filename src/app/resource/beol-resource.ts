7ea6db2591411aae5833583b6e7298f6e8c3d68f
André Kilchenmann <github@milchkannen.ch>
2019-09-02 11:16:56 +0200

    fix: Bug fix after Angular 8 update

--- src/app/resource/beol-resource.ts
+++ src/app/resource/beol-resource.ts
@@ -18,7 +18,7 @@ import { StillImageComponent } from '@knora/viewer';
 
 import { Subscription } from 'rxjs';
 
-import { OnDestroy, OnInit, ViewChild, Directive } from '@angular/core';
+import { OnDestroy, OnInit, ViewChild } from '@angular/core';
 
 import { BeolService } from '../services/beol.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
@@ -31,9 +31,6 @@ export interface PropertyValues {
     [index: string]: ReadPropertyItem[];
 }
 
-@Directive({
-    selector: 'appBeolResource'
-})
 export abstract class BeolResource implements OnInit, OnDestroy {
 
     abstract iri: string;
