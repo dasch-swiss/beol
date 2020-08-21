82540379f18b12a9c45b0938a7a013b3f3456f21
flaurens <flavie.laurens@unibas.ch>
2020-08-21 11:15:53 +0200

    refactor(simpleResource): Update some imports and selectors

--- src/app/resource/simpleResource/simpleResource.component.ts
+++ src/app/resource/simpleResource/simpleResource.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import { IncomingService, KnoraConstants, OntologyCacheService, OntologyInformation, ReadResource, ResourceService } from '@knora/core';
-import { BeolResource } from './../beol-resource';
+import { Component, Inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {
+    Constants,
+    KnoraApiConnection,
+    ReadResource,
+    ResourceClassAndPropertyDefinitions
+} from '@dasch-swiss/dsp-js';
+import { DspApiConnectionToken } from '@dasch-swiss/dsp-ui';
+import { OntologyCacheService } from '@knora/core';
 import { Subscription } from 'rxjs';
+import { IncomingService } from 'src/app/services/incoming.service';
 import { BeolService } from '../../services/beol.service';
+import { BeolResource } from './../beol-resource';
 
 @Component({
     selector: 'app-simple-resource',
@@ -15,23 +23,24 @@ export class SimpleResourceComponent extends BeolResource {
 
     iri: string;
     resource: ReadResource;
-    ontologyInfo: OntologyInformation;
+    ontologyInfo: ResourceClassAndPropertyDefinitions;
     incomingStillImageRepresentationCurrentOffset: number; // last offset requested for `this.resource.incomingStillImageRepresentations`
     isLoading = true;
     errorMessage: any;
-    KnoraConstants = KnoraConstants;
+    DspConstants = Constants;
     navigationSubscription: Subscription;
 
     propIris;
 
-    constructor(protected _route: ActivatedRoute,
-                protected _resourceService: ResourceService,
-                protected _cacheService: OntologyCacheService,
-                protected _incomingService: IncomingService,
-                public location: Location,
-                protected _beolService: BeolService) {
+    constructor(
+        @Inject(DspApiConnectionToken) protected _dspApiConnection: KnoraApiConnection,
+        protected _route: ActivatedRoute,
+        protected _cacheService: OntologyCacheService,
+        protected _incomingService: IncomingService,
+        public location: Location,
+        protected _beolService: BeolService) {
 
-        super(_route, _resourceService, _cacheService, _incomingService, _beolService);
+        super(_dspApiConnection, _route, _cacheService, _incomingService, _beolService);
     }
 
     initProps() {
