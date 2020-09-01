94e51fd4756f1a16f7c78b2b4a6561b791bcfa49
Tobias Schweizer <t.schweizer@unibas.ch>
2020-09-01 16:27:25 +0200

    test (app comp): adapt spec file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,49 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material-module';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ReadListValue } from '@dasch-swiss/dsp-js';
+
+/**
+ * Test host component to simulate dsp-search-panel component.
+ */
+@Component({
+    selector: 'dsp-search-panel',
+    template: ''
+})
+class DspSearchPanelComponent {
+
+}
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        MaterialModule,
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-      providers: []
-    }).compileComponents();
-  }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  /* it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to beol!');
-  })); */
+
+    beforeEach(async(() => {
+
+        TestBed.configureTestingModule({
+            imports: [
+                MaterialModule,
+                RouterTestingModule
+            ],
+            declarations: [
+                AppComponent,
+                DspSearchPanelComponent
+            ],
+            providers: []
+        }).compileComponents();
+
+
+    }));
+
+    it('should create the app', async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+    }));
+
+    it(`should have as title 'app'`, async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app.title).toEqual('app');
+    }));
 });
