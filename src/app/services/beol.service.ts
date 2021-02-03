767f6d276fb168d41d2914d1f6c54d1162bef0c4
Sepideh Alassi <sepideh.alassi@unibas.ch>
2021-02-03 17:21:18 +0100

    fix (gravsearch) optimize queries (#183)

--- src/app/services/beol.service.ts
+++ src/app/services/beol.service.ts
@@ -97,11 +97,7 @@ export class BeolService {
     } WHERE {
 
         ?introSection a beol:section .
-      	?introSection a knora-api:Resource .
         ?introSection beol:beolIDs ?sectionId .
-
-        beol:beolIDs knora-api:objectType xsd:string .
-        ?sectionId a xsd:string .
 
         FILTER(?sectionId = "${id}")
 
@@ -116,97 +112,125 @@ export class BeolService {
 
 
     /**
-     * Creates the Gravsearch needed for the search for the correspodence between two persons, ordered by date.
+     * Creates the Gravsearch needed for the search for the LEOO correspodence ordered by date.
      *
      * @param gnd1 the GND/IAF identifier for the first correspondent.
      * @param gnd2 the GND/IAF identifier for the second correspondent.
-     * @param noTranslations indicates if the letter is in original language (not a translation).
+     * @param showTranslation indicates if the translation of the letter should be shown or in original language.
      * @param offset the offset to be used.
      */
-    searchForCorrespondence(gnd1: string, gnd2: string, noTranslations: boolean = false, offset: number = 0): string {
+    searchForLeooCorrespondence(gnd1: string, gnd2: string, showTranslation: boolean = false, offset: number = 0): string {
 
-        let language = '';
+        let resourcesThatHaveNoTranslation = '';
 
-        if (noTranslations) {
-            // original language: must have the property: beol:letterHasTranslation
+        if (showTranslation) {
+            // The resources representing translations of LEOO letters do not have the property: beol:letterHasTranslation.
 
-            language = `
-    ?letter beol:letterHasTranslation ?translation .
+            resourcesThatHaveNoTranslation = `
 
-    beol:letterHasTranslation knora-api:objectType knora-api:Resource .
-    ?translation a knora-api:Resource .
+            FILTER NOT EXISTS {
+                ?letter beol:letterHasTranslation ?translation .
+            } `;
 
-            `;
         } else {
-            // translation: must not have the property: beol:letterHasTranslation
-
-            language = `
-
-    FILTER NOT EXISTS {
-        ?letter beol:letterHasTranslation ?translation .
-    }
-
-    beol:letterHasTranslation knora-api:objectType knora-api:Resource .
-    ?translation a knora-api:Resource .
-
+            // The resources representing the LEOO letters in original language have the property: beol:letterHasTranslation.
+            resourcesThatHaveNoTranslation = `
+            ?letter beol:letterHasTranslation ?translation .
             `;
-
         }
 
         const correspondenceTemplate = `
-    PREFIX beol: <${this._appInitService.config['ontologyIRI']}/ontology/0801/beol/simple/v2#>
-    PREFIX knora-api: <http://api.knora.org/ontology/knora-api/simple/v2#>
+            PREFIX beol: <${this._appInitService.config['ontologyIRI']}/ontology/0801/beol/v2#>
+            PREFIX knora-api: <http://api.knora.org/ontology/knora-api/v2#>
+        
+            CONSTRUCT {
+                ?letter knora-api:isMainResource true .
+                ?letter ?linkingProp1  ?person1 .
+                ?letter ?linkingProp2  ?person2 .
+                ?letter beol:creationDate ?date .
+                
+            } WHERE {
+            
+               ?gnd1 knora-api:valueAsString "${gnd1}" .
+               ?person1 beol:hasIAFIdentifier ?gnd1 .
+               
+               ?gnd2 knora-api:valueAsString "${gnd2}" .
+               ?person2 beol:hasIAFIdentifier ?gnd2 .
+               
+                ?letter ?linkingProp1 ?person1 .
+                FILTER(?linkingProp1 = beol:hasAuthor || ?linkingProp1 = beol:hasRecipient )
+        
+                ?letter ?linkingProp2 ?person2 .
+                FILTER(?linkingProp2 = beol:hasAuthor || ?linkingProp2 = beol:hasRecipient )
+                
+                ?letter beol:creationDate ?date .
+                ${resourcesThatHaveNoTranslation}
+                
+            } ORDER BY ?date
+        `;
 
-    CONSTRUCT {
-        ?letter knora-api:isMainResource true .
+        // offset component of the Gravsearch query
+        const offsetTemplate = `
+        OFFSET ${offset}
+        `;
 
-        ?letter beol:creationDate ?date .
+        // function that generates the same Gravsearch query with the given offset
+        const generateGravsearchWithCustomOffset = (localOffset: number): string => {
+            const offsetCustomTemplate = `
+            OFFSET ${localOffset}
+            `;
 
-        ?letter ?linkingProp1  ?person1 .
+            return correspondenceTemplate + offsetCustomTemplate;
+        };
 
-        ?letter ?linkingProp2  ?person2 .
+        if (offset === 0) {
+            // store the function so another Gravsearch query can be created with an increased offset
+            this._searchParamsService.changeSearchParamsMsg(new AdvancedSearchParams(generateGravsearchWithCustomOffset));
+        }
 
-    } WHERE {
-        ?letter a knora-api:Resource .
-        ?letter a beol:letter .
+        // console.log(correspondenceTemplate + offsetTemplate);
 
-        ${language}
+        return correspondenceTemplate + offsetTemplate;
+    }
 
-        ?letter beol:creationDate ?date .
+    /**
+     * Creates the Gravsearch needed for the search for the correspodence between two persons, ordered by date  (LECE and BEBB editions).
+     *
+     * @param gnd1 the GND/IAF identifier for the first correspondent.
+     * @param gnd2 the GND/IAF identifier for the second correspondent.
+     * @param offset the offset to be used.
+     */
+    searchForCorrespondence(gnd1: string, gnd2: string, offset: number = 0): string {
 
-        beol:creationDate knora-api:objectType knora-api:Date .
-        ?date a knora-api:Date .
 
-        ?letter ?linkingProp1  ?person1 .
-
-        ?linkingProp1 knora-api:objectType knora-api:Resource .
-        FILTER(?linkingProp1 = beol:hasAuthor || ?linkingProp1 = beol:hasRecipient )
-
-        ?person1 a beol:person .
-        ?person1 a knora-api:Resource .
-
-        ?person1 beol:hasIAFIdentifier ?gnd1 .
-        FILTER(?gnd1 = "${gnd1}")
-
-        ?gnd1 a xsd:string .
-
-        ?letter ?linkingProp2 ?person2 .
-        ?linkingProp2 knora-api:objectType knora-api:Resource .
-
-        FILTER(?linkingProp2 = beol:hasAuthor || ?linkingProp2 = beol:hasRecipient )
-
-        ?person2 a beol:person .
-        ?person2 a knora-api:Resource .
-
-        ?person2 beol:hasIAFIdentifier ?gnd2 .
-        FILTER(?gnd2 = "${gnd2}")
-
-        ?gnd2 a xsd:string .
-
-        beol:hasIAFIdentifier knora-api:objectType xsd:string .
-
-    } ORDER BY ?date
-`;
+        const correspondenceTemplate = `
+            PREFIX beol: <${this._appInitService.config['ontologyIRI']}/ontology/0801/beol/v2#>
+            PREFIX knora-api: <http://api.knora.org/ontology/knora-api/v2#>
+        
+            CONSTRUCT {
+                ?letter knora-api:isMainResource true .
+                ?letter ?linkingProp1  ?person1 .
+                ?letter ?linkingProp2  ?person2 .
+                ?letter beol:creationDate ?date .
+                
+            } WHERE {
+            
+               ?gnd1 knora-api:valueAsString "${gnd1}" .
+               ?person1 beol:hasIAFIdentifier ?gnd1 .
+               
+               ?gnd2 knora-api:valueAsString "${gnd2}" .
+               ?person2 beol:hasIAFIdentifier ?gnd2 .
+               
+                ?letter ?linkingProp1 ?person1 .
+                FILTER(?linkingProp1 = beol:hasAuthor || ?linkingProp1 = beol:hasRecipient )
+        
+                ?letter ?linkingProp2 ?person2 .
+                FILTER(?linkingProp2 = beol:hasAuthor || ?linkingProp2 = beol:hasRecipient )
+                
+                ?letter beol:creationDate ?date .
+                
+            } ORDER BY ?date
+        `;
 
         // offset component of the Gravsearch query
         const offsetTemplate = `
@@ -245,8 +269,6 @@ export class BeolService {
                 ?letter knora-api:isMainResource true .
 
             } WHERE {
-
-                ?letter a knora-api:Resource .
                 ?letter a newton:letter .
 
             }
@@ -275,7 +297,7 @@ export class BeolService {
     }
 
     /**
-     * Creates the Gravsearch needed for the search for the newton correspodence.
+     * Creates the Gravsearch needed for the search for the leibniz correspodence.
      */
     searchForLeibnizCorrespondence(offset: number = 0): string {
 
@@ -287,8 +309,6 @@ export class BeolService {
                 ?letter knora-api:isMainResource true .
 
             } WHERE {
-
-                ?letter a knora-api:Resource .
                 ?letter a leibniz:letter .
 
             }
@@ -324,8 +344,8 @@ export class BeolService {
     searchForLetterFromLEOO(repertoriumNumber: string): string {
 
         const letterByNumberTemplate = `
-        PREFIX beol: <${this._appInitService.config['ontologyIRI']}/ontology/0801/beol/simple/v2#>
-        PREFIX knora-api: <http://api.knora.org/ontology/knora-api/simple/v2#>
+        PREFIX beol: <${this._appInitService.config['ontologyIRI']}/ontology/0801/beol/v2#>
+        PREFIX knora-api: <http://api.knora.org/ontology/knora-api/v2#>
         CONSTRUCT {
 
             ?letter knora-api:isMainResource true .
@@ -333,15 +353,9 @@ export class BeolService {
 
         } WHERE {
 
-            ?letter a knora-api:Resource .
-
             ?letter a beol:letter .
-
-            ?letter beol:letterHasRepertoriumNumber ?letterNumber .
-            beol:letterHasRepertoriumNumber knora-api:objectType <http://www.w3.org/2001/XMLSchema#string> .
-            ?letterNumber a <http://www.w3.org/2001/XMLSchema#string> .
-
-            FILTER(?letterNumber = "${repertoriumNumber}"^^<http://www.w3.org/2001/XMLSchema#string>)
+            ?repertoriumNr knora-api:valueAsString "${repertoriumNumber}" .
+            ?letter beol:letterHasRepertoriumNumber ?repertoriumNr .
 
         }
 
