f166e73661f04c81fe476abd6733812cfa5c2cb7
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2023-11-30 10:09:07 +0100

    fix unit test

--- src/app/dsp-ui-lib/viewer/values/boolean-value/boolean-value.component.spec.ts
+++ src/app/dsp-ui-lib/viewer/values/boolean-value/boolean-value.component.spec.ts
@@ -168,7 +168,7 @@ describe('BooleanValueComponent', () => {
       valueBooleanNativeElement = valueBooleanDebugElement.nativeElement;
 
       checkboxEl = valueBooleanDebugElement.query(By.css('input[type="checkbox"]')).nativeElement;
-      checkboxLabel = valueBooleanDebugElement.query(By.css('span[class="mat-checkbox-label"]')).nativeElement;
+      checkboxLabel = valueBooleanDebugElement.query(By.css('label')).nativeElement;
 
       commentBooleanDebugElement = valueComponentDe.query(By.css('textarea.comment'));
       commentBooleanNativeElement = commentBooleanDebugElement.nativeElement;
