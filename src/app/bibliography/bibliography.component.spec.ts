7d181bdd68b83075ee9021e2df7a90e95b66f7b1
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2023-12-07 13:18:15 +0100

    fix unit test

--- src/app/bibliography/bibliography.component.spec.ts
+++ src/app/bibliography/bibliography.component.spec.ts
@@ -1,23 +1,41 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { BibliographyComponent } from './bibliography.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BeolService } from '../services/beol.service';
+import { AppInitService } from '../dsp-ui-lib/core';
 
 describe('BibliographyComponent', () => {
-  let component: BibliographyComponent;
-  let fixture: ComponentFixture<BibliographyComponent>;
+    let component: BibliographyComponent;
+    let fixture: ComponentFixture<BibliographyComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ BibliographyComponent ]
-    })
-    .compileComponents();
+    beforeEach(waitForAsync(() => {
 
-    fixture = TestBed.createComponent(BibliographyComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        const appInitServiceMock = {
+            config: {
+                ontologyIRI: 'http://0.0.0.0:3333'
+            }
+        };
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        const beolServiceSpy = jasmine.createSpyObj('BeolService', ['getEntriesForManuscript']); // see https://angular.io/guide/testing#angular-testbed
+
+        TestBed.configureTestingModule({
+            imports: [
+                RouterTestingModule,
+            ],
+            declarations: [BibliographyComponent],
+            providers: [
+                { provide: BeolService, useValue: beolServiceSpy },
+                { provide: AppInitService, useValue: appInitServiceMock }
+            ]
+        })
+            .compileComponents();
+
+        fixture = TestBed.createComponent(BibliographyComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    }));
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
